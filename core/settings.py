f5ef85df0ecd4448d1065404f2ac6a1cb900e6f9
Gaston Franco <gastonfr24@gmail.com>
2022-10-22 21:37:36 -0300

    settings-update

--- core/settings.py
+++ core/settings.py
@@ -233,13 +233,13 @@ ACTIVE_CAMPAIGN_KEY=os.environ.get('ACTIVE_CAMPAIGN_KEY')
 
 # AWS y Mail
 if not DEBUG:
-    DEFAULT_FROM_EMAIL="Uridium <mail@uridium.network>"
+    DEFAULT_FROM_EMAIL="LemonPy <email@lemon-py.com>"
     EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    #EMAIL_HOST = env('EMAIL_HOST')
+    #EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    #EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    #EMAIL_PORT = env('EMAIL_PORT')
+    #EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
     
     # django-ckeditor will not work with S3 through django-storages without this line in settings.py
