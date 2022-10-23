61a06f600efee9f8250c5c0fc2847f8ef4e3e591
Gaston Franco <gastonfr24@gmail.com>
2022-10-23 14:12:38 -0300

    deploy

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -3,7 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'private'
+    #default_acl = 'private'
+    default_acl = 'public-read'
 
 
 class MediaStore(S3Boto3Storage):
