167db8c7cc4dc4e935531f61622a7e3c7f550b48
Gaston Franco <gastonfr24@gmail.com>
2022-10-25 21:50:33 -0300

    front

--- src/components/squeletons/ProjectSqlt.jsx
+++ src/components/squeletons/ProjectSqlt.jsx
@@ -4,7 +4,7 @@ function ProjectSqlt() {
   return (
 
             
-<div className="bg-zinc-900 animate-pulse shadow-lg shadow-[#040c16] container rounded-md flex justify-center text-center items-center mx-auto content-div-sqlt">
+<div className="bg-gray-200 dark:bg-zinc-900 animate-pulse shadow-lg shadow-[#040c16] container rounded-md flex justify-center text-center items-center mx-auto content-div-sqlt">
 
 </div>
 
